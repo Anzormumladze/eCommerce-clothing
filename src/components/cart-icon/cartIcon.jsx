37bc5c851c3ac2c88767d9e9a95a33eebd67e1f8
Anzormumladze <anzor.anzor.mumladze@gmail.com>
2020-03-12 15:55:08 +0400

    adding selector in our app ,created cartSelectors in redux

--- src/components/cart-icon/cartIcon.jsx
+++ src/components/cart-icon/cartIcon.jsx
@@ -3,15 +3,21 @@ import "./cartIcon.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/cart.svg";
 import { connect } from "react-redux";
 import { toggleCartHiden } from "../../redux/cart/cartActions";
-const CartIcon = ({ toggleCartHiden }) => {
+import { selectCartItemsCount } from "../../redux/cart/cartSelectors";
+const CartIcon = ({ toggleCartHiden, itemCount }) => {
+  console.log(itemCount);
   return (
     <div className="cart-icon" onClick={toggleCartHiden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
 const mapDispatchToProps = dispatch => ({
   toggleCartHiden: () => dispatch(toggleCartHiden())
 });
-export default connect(null, mapDispatchToProps)(CartIcon);
+
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
